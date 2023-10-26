62a2aff38ed66eb4a4dfc9ed6c5b1371a41e4f3b
ElTinMar <martin.privat@hotmail.fr>
2023-10-26 17:30:19 +0200

    move widgets

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 __all__ = ["background", "video_display", "video_processor", 
-           "video_reader", "video_writer"]
+           "video_reader", "video_writer", "background_widget", "playlist_widget"]
 
 from .background import *
 from .video_display import *
 from .video_processor import *
 from .video_reader import *
 from .video_writer import *
+from .background_widget import *
+from .playlist_widget import *
--- background_widget.py
+++ background_widget.py
@@ -3,8 +3,9 @@
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMainWindow, QFileDialog, QPushButton, QLineEdit, QComboBox, QStackedWidget, QLabel, QVBoxLayout, QHBoxLayout, QWidget
-from .. import BackgroundSubtractor, NoBackgroundSub, BackroundImage, StaticBackground, DynamicBackground, DynamicBackgroundMP, OpenCV_VideoReader
-from ..qt_widgets import LabeledSpinBox, FileOpenLabeledEditButton, NDarray_to_QPixmap
+from .video_reader import OpenCV_VideoReader
+from .background import BackgroundSubtractor, NoBackgroundSub, BackroundImage, StaticBackground, DynamicBackground, DynamicBackgroundMP
+from .qt_widgets import LabeledSpinBox, FileOpenLabeledEditButton, NDarray_to_QPixmap
 import os
 import cv2
 import numpy as np
