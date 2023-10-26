ff81848b4278c80a40fb35426e52c8c99e1e04a5
ElTinMar <martin.privat@hotmail.fr>
2023-10-26 12:03:06 +0200

    remove widgets

--- __init__.py
+++ __init__.py
@@ -1,9 +1,6 @@
-__all__ = ["background_widget", "background", "playlist_widget", 
-           "video_display", "video_processor", "video_reader",
-           "video_writer"]
+__all__ = ["background", "video_display", "video_processor", 
+           "video_reader", "video_writer"]
 
-from .background_widget import *
-from ..gui.playlist_widget import *
 from .background import *
 from .video_display import *
 from .video_processor import *
