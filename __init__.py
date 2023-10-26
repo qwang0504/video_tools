06812a34d79bde8d66af6cac6242549197c0082a
ElTinMar <martin.privat@hotmail.fr>
2023-10-26 11:21:13 +0200

    adding imports

---
+++ __init__.py
@@ -0,0 +1,11 @@
+__all__ = ["background_widget", "background", "playlist_widget", 
+           "video_display", "video_processor", "video_reader",
+           "video_writer"]
+
+from .background_widget import *
+from .playlist_widget import *
+from .background import *
+from .video_display import *
+from .video_processor import *
+from .video_reader import *
+from .video_writer import *
