72bf8a4e8c7ccf6f42b3af0b00d4e46e97311ac8
ElTinmar <martin.privat@hotmail.fr>
2023-11-08 11:36:43 +0100

    update file

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='ipc_tools',
+    name='video_tools',
     author='Martin Privat',
     version='0.1',
     packages=['video_tools','video_tools.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    description='share numpy arrays between processes',
+    description='simple video reader, writer, and processing functions',
     long_description=open('README.md').read(),
     install_requires=[
         "numpy", 
