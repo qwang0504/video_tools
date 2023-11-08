4db4657a0dc31cd3888e5b92c5708c7537604fca
ElTinmar <martin.privat@hotmail.fr>
2023-11-08 11:26:21 +0100

    package

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # video_tools
 
 tools to handle videos reading, writing and processing
+
+```
+pip install git+https://github.com/ElTinmar/video_tools.git@main
+```
--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ setup(
     name='ipc_tools',
     author='Martin Privat',
     version='0.1',
-    packages=['ipc_tools','ipc_tools.tests'],
+    packages=['video_tools','video_tools.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='share numpy arrays between processes',
     long_description=open('README.md').read(),
     install_requires=[
         "numpy", 
-        "pyzmq",
-        "arrayqueues",
+        "scipy",
+        "PyQt5",
         "opencv-python",
-        "pandas",
-        "seaborn",
-        "tqdm",
-        "matplotlib",
-        "scipy"
+        "qt_widgets",
+        "image_tools",
+        "tqdm"
     ]
 )
