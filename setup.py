d7fb19577d0a708632040325ccb665ea4924c93e
ElTinMar <martin.privat@hotmail.fr>
2023-11-24 20:00:47 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='video_tools',
     author='Martin Privat',
-    version='0.1.10',
+    version='0.1.11',
     packages=['video_tools','video_tools.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='simple video reader, writer, and processing functions',
