239be0a3c3fca85ca737f54f523bd15da274f065
ElTinMar <martin.privat@hotmail.fr>
2023-12-11 19:49:30 +0100

    update opencv to custom whl

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='video_tools',
     author='Martin Privat',
-    version='0.1.12',
+    version='0.1.13',
     packages=['video_tools','video_tools.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='simple video reader, writer, and processing functions',
@@ -12,7 +12,7 @@ setup(
         "numpy", 
         "scipy",
         "PyQt5",
-        "opencv-python",
+        "opencv-contrib-python-rolling @ https://github.com/ElTinmar/build_opencv/raw/main/opencv_contrib_python_rolling-4.8.0.20231210-cp38-cp38-linux_x86_64.whl",
         "qt_widgets @ git+https://github.com/ElTinmar/qt_widgets.git@main",
         "image_tools @ git+https://github.com/ElTinmar/image_tools.git@main",
         "tqdm"
