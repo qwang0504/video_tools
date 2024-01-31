93bdac184fd14a70486c8d2fee7d6dc08fa3c301
ElTinmar <martin.privat@hotmail.fr>
2024-01-31 13:47:25 +0100

    rework video reader

--- video_tools/video_reader.py
+++ video_tools/video_reader.py
@@ -1,12 +1,96 @@
 import cv2
 from numpy.typing import NDArray
 import numpy as np
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Dict
 from multiprocessing import Queue, Process, Event 
+from abc import ABC
 
 # TODO: add resizing as an option 
 # TODO: Check index error (+-1). Make sure that number of frames is correct (end index valid)
-class OpenCV_VideoReader:
+
+class VideoReader(ABC):
+
+    def open_file(            
+            filename: str, 
+            safe: bool = False
+        ) -> None:
+        pass
+
+    def next_frame(self) -> Tuple[bool,NDArray]:
+        pass
+        
+    def get_fps(self) -> float:
+        pass
+    
+    def get_width(self) -> int:
+        pass
+    
+    def get_height(self) -> int:
+        pass
+    
+    def get_num_channels(self) -> int:
+        pass
+
+    def get_number_of_frame(self) -> int:
+        pass
+
+def get_number_of_frames(filename: str, safe: bool = True):
+
+    cap = cv2.VideoCapture(filename)
+
+    # number of frames
+    if safe:
+        # loop over the whole video and count each frame
+        counter = 0
+        while True:
+            rval, frame = cap.read()
+            if not rval:
+                break
+            counter = counter + 1
+        number_of_frames = counter
+
+    else:
+        # Trust opencv to return video properties. This is fast but there are known issues 
+        # with this. Use at your own risk.
+        number_of_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    cap.release()
+    return number_of_frames 
+
+def get_fps(filename: str) -> int:
+    cap = cv2.VideoCapture(filename)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    cap.release()
+    return fps
+
+def get_image_properties(filename: str, safe: bool = True) -> Tuple[int, int, int, np.dtype]:
+
+    cap = cv2.VideoCapture(filename)
+    rval, frame = cap.read()
+    if not rval:
+        raise RuntimeError('Could not read first frame')
+
+    height, width, num_channels = frame.shape
+    data_type = frame.dtype
+
+    cap.release()
+    return (height, width, num_channels, data_type)
+
+def get_video_info(filename: str, safe: bool = True) -> Dict:
+    num_frames = get_number_of_frames(filename, safe)
+    fps = get_fps(filename)
+    height, width, num_channels, data_type = get_image_properties(filename, safe)
+    info = {
+        'height': height,
+        'width': width,
+        'num_channels': num_channels,
+        'data_type': data_type,
+        'fps': fps,
+        'num_frames': num_frames,
+    }
+    return info
+
+class OpenCV_VideoReader(VideoReader):
     
     def __init__(self):
         self.open = False
@@ -15,66 +99,36 @@ class OpenCV_VideoReader:
             self, 
             filename: str, 
             safe: bool = False, 
+            resize: float = 1,
             crop: Optional[Tuple[int,int,int,int]] = None,
-            resize: Optional[float] = None,
             backend: Optional[int] = None
         ) -> None:
             
+        # store param
         self._filename = filename
+        self._safe = safe
+        self._current_frame = 0
+        self._crop = crop # [left,bottom,width,height]
+        self._resize = resize
         self._backend = backend
+
+        # get video metadata 
+        self._video_info = get_video_info(filename, safe)
+        self._number_of_frames = self._video_info['num_frames']
+        self._fps = self._video_info['fps']
+        self._num_channels = self._video_info['num_channels']
+        self._type = self._video_info['data_type']
+        if crop is not None:
+            self._width = int(crop[2] * resize)
+            self._height = int(crop[3] * resize)
+        else:
+            self._width = self._video_info['width'] * resize
+            self._height = self._video_info['height'] * resize
+            
         if backend is not None:
             self._capture = cv2.VideoCapture(filename, backend)
         else:
             self._capture = cv2.VideoCapture(filename)
-        self._current_frame = 0
-        self._number_of_frames = 0
-        self._width = 0
-        self._height = 0
-        self._fps = 0
-        self._num_channels = 0
-        self._safe = safe
-        self._crop = crop # [left,bottom,width,height]
-        self._resize = resize
-            
-        # count number of frames
-        if safe:
-            # loop over the whole video and count each frame
-            counter = 0
-            while True:
-                rval, frame = self._capture.read()
-                if not rval:
-                    break
-                counter = counter + 1
-            self._number_of_frames = counter
-
-            # reinitialize reader
-            self._capture.release()
-            self._capture = cv2.VideoCapture(filename)
-        else:
-            # Trust opencv to return video properties. This is fast but there are known issues 
-            # with this. Use at your own risk.
-            self._number_of_frames = int(self._capture.get(cv2.CAP_PROP_FRAME_COUNT))
-
-        self._fps = self._capture.get(cv2.CAP_PROP_FPS)
-
-        # get video properties from first frame
-        rval, frame = self._capture.read()
-        if not rval:
-            raise(RuntimeError(f"Error while reading the first frame")) 
-        
-        if self._crop is not None:
-            self._width = self._crop[2]
-            self._height = self._crop[3]
-        else:
-            self._width = frame.shape[1]
-            self._height = frame.shape[0]
-        if len(frame.shape) > 2:
-            self._num_channels = frame.shape[2]
-        else:
-            self._num_channels = 1
-        self._type = frame.dtype  
-
-        self.reset_reader()
         self.open = True
 
     def is_open(self) -> bool:
@@ -103,7 +157,7 @@ class OpenCV_VideoReader:
                     self._crop[1]:self._crop[1]+self._crop[3],
                     self._crop[0]:self._crop[0]+self._crop[2]
                 ]
-            if self._resize is not None:
+            if self._resize != 1:
                 frame = cv2.resize(
                     frame,
                     None,
@@ -193,10 +247,10 @@ class OpenCV_VideoReader:
         cv2.destroyAllWindows()
 
     def get_width_max(self) -> int:
-        return int(self._capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        return self._video_info['width']
 
     def get_height_max(self) -> int:
-        return int(self._capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        return self._video_info['height']
     
     def get_fps(self) -> float:
         return self._fps
@@ -222,7 +276,7 @@ class OpenCV_VideoReader:
     def get_number_of_frame(self) -> int:
         return self._number_of_frames
 
-class Buffered_OpenCV_VideoReader(Process):
+class Buffered_OpenCV_VideoReader(Process, VideoReader):
     '''
     Buffer video file reading. Cannot seek to a specific frame. Faster 
     when you need to process each video frame sequentially
@@ -232,66 +286,35 @@ class Buffered_OpenCV_VideoReader(Process):
             self, 
             filename: str, 
             safe: bool = False, 
+            resize: float = 1,
             crop: Optional[Tuple[int,int,int,int]] = None,
             backend: Optional[int] = None
         ):
             
+        # store param
         self._filename = filename
-        self._backend = backend
-        if backend is not None:
-            self._capture = cv2.VideoCapture(filename)
-        else:
-            self._capture = cv2.VideoCapture(filename, backend)
-        self._current_frame = 0
-        self._number_of_frames = 0
-        self._width = 0
-        self._height = 0
-        self._fps = 0
-        self._num_channels = 0
         self._safe = safe
+        self._current_frame = 0
         self._crop = crop # [left,bottom,width,height]
-        self._queue = Queue(maxsize=100)
+        self._resize = resize
+        self._backend = backend
+
+        # get video metadata 
+        self._video_info = get_video_info(filename, safe)
+        self._number_of_frames = self._video_info['num_frames']
+        self._fps = self._video_info['fps']
+        self._num_channels = self._video_info['num_channels']
+        self._type = self._video_info['data_type']
+        if crop is not None:
+            self._width = int(crop[2] * resize)
+            self._height = int(crop[3] * resize)
+        else:
+            self._width = self._video_info['width'] * resize
+            self._height = self._video_info['height'] * resize
+
+        # multiprocessing
+        self._queue = Queue(maxsize=100) 
         self._stop = Event()
-            
-        # count number of frames
-        if safe:
-            # loop over the whole video and count each frame
-            counter = 0
-            while True:
-                rval, frame = self._capture.read()
-                if not rval:
-                    break
-                counter = counter + 1
-            self._number_of_frames = counter
-
-            # reinitialize reader
-            self._capture.release()
-            self._capture = cv2.VideoCapture(filename)
-        else:
-            # Trust opencv to return video properties. This is fast but there are known issues 
-            # with this. Use at your own risk.
-            self._number_of_frames = int(self._capture.get(cv2.CAP_PROP_FRAME_COUNT))
-
-        self._fps = self._capture.get(cv2.CAP_PROP_FPS)
-
-        # get video properties from first frame
-        rval, frame = self._capture.read()
-        if not rval:
-            raise(RuntimeError(f"Error while reading the first frame")) 
-        
-        if self._crop is not None:
-            self._width = self._crop[2]
-            self._height = self._crop[3]
-        else:
-            self._width = frame.shape[1]
-            self._height = frame.shape[0]
-        if len(frame.shape) > 2:
-            self._num_channels = frame.shape[2]
-        else:
-            self._num_channels = 1
-        self._type = frame.dtype  
-
-        self._capture.release()
 
     def run(self) -> None:
         if self._backend is not None:
@@ -312,6 +335,7 @@ class Buffered_OpenCV_VideoReader(Process):
         self._stop.set()
 
     def read_frame(self) -> Tuple[bool,NDArray]:
+
         rval, frame = self._capture.read()
         if rval:
             self._current_frame = self._current_frame + 1
@@ -320,12 +344,16 @@ class Buffered_OpenCV_VideoReader(Process):
                     self._crop[1]:self._crop[1]+self._crop[3],
                     self._crop[0]:self._crop[0]+self._crop[2]
                 ]
+            if self._resize != 1:
+                frame = cv2.resize(
+                    frame,
+                    None,
+                    None,
+                    self._resize,
+                    self._resize,
+                    cv2.INTER_NEAREST
+                )
         return (rval, frame)
-    
-    def reset_reader(self) -> None:
-        self._capture.release()
-        self._capture = cv2.VideoCapture(self._filename)
-        self._current_frame = 0
 
     def get_width(self) -> int:
         return self._width
@@ -350,3 +378,135 @@ class Buffered_OpenCV_VideoReader(Process):
     
     def get_number_of_frame(self) -> int:
         return self._number_of_frames
+    
+class InMemory_VideoReader(VideoReader):
+    '''
+    Decode video file and buffer raw frames in memory.
+    This is useful when you want to test the performance 
+    of a code that acts on frames but don't want to be limited 
+    by how fast you can read your video file.
+    Please be aware that for large pixel counts, raw frames take
+    up a lot of space.
+    '''
+    
+    def __init__(self):
+        self.open = False
+
+    def open_file(
+            self, 
+            filename: str, 
+            safe: bool = False, 
+            memsize_bytes: int = 1e9, # one GB by default
+            resize: float = 1,
+            crop: Optional[Tuple[int,int,int,int]] = None,
+            backend: Optional[int] = None
+        ) -> None:
+            
+        # store param
+        self._filename = filename
+        self._safe = safe
+        self._memsize_bytes = memsize_bytes
+        self._current_frame = 0
+        self._crop = crop # [left,bottom,width,height]
+        self._resize = resize
+        self._backend = backend
+
+        # get video metadata 
+        self._video_info = get_video_info(filename, safe)
+        self._number_of_frames = self._video_info['num_frames']
+        self._fps = self._video_info['fps']
+        self._num_channels = self._video_info['num_channels']
+        self._type = self._video_info['data_type']
+        if crop is not None:
+            self._width = int(crop[2] * resize)
+            self._height = int(crop[3] * resize)
+        else:
+            self._width = self._video_info['width'] * resize
+            self._height = self._video_info['height'] * resize
+            
+        # compute number of frames
+        self._num_buffered_frames = min(
+            self._number_of_frames,
+            self._memsize_bytes / (self._width*self._height*self._num_channels)
+        )
+        
+        # preallocate numpy array
+        self._mem_buffer = np.zeros(
+            (self._height, self._width, self._num_channels, self._num_buffered_frames), 
+            dtype = self._type
+        )
+
+        # open video capture
+        if backend is not None:
+            self._capture = cv2.VideoCapture(filename, backend)
+        else:
+            self._capture = cv2.VideoCapture(filename)
+        self.open = True
+
+        # load frames from file into memory
+        for i in range(self._num_buffered_frames): 
+            rval, frame = self.read()
+            if not rval:
+                raise RuntimeError('Unable to buffer all frames')
+            self._mem_buffer[:,:,:,i] = frame
+
+        self.open = True
+
+    def is_open(self) -> bool:
+        return self.open
+    
+    def reset_reader(self) -> None:
+        self._current_frame = 0
+
+    def next_frame(self) -> Tuple[bool, Optional[NDArray]]:
+        self._current_frame += 1
+        if self._current_frame <= self._num_buffered_frames:
+            return (True, self._mem_buffer[:,:,:,self._current_frame]) 
+        else:
+            return (False, None)
+
+    def read(self) -> Tuple[bool,NDArray]:
+        """
+        TODO
+        """
+        rval, frame = self._capture.read()
+        if rval:
+            if self._crop is not None:
+                frame = frame[
+                    self._crop[1]:self._crop[1]+self._crop[3],
+                    self._crop[0]:self._crop[0]+self._crop[2]
+                ]
+            if self._resize is not None:
+                frame = cv2.resize(
+                    frame,
+                    None,
+                    None,
+                    self._resize,
+                    self._resize,
+                    cv2.INTER_NEAREST
+                )
+        return (rval, frame)
+    
+    def get_fps(self) -> float:
+        return self._fps
+    
+    def get_width(self) -> int:
+        return self._width
+    
+    def get_height(self) -> int:
+        return self._height
+    
+    def get_num_channels(self) -> int:
+        return self._num_channels
+    
+    def get_type(self) -> np.dtype:
+        return self._type
+    
+    def get_current_frame_index(self) -> int:
+        return self._current_frame
+
+    def get_filename(self) -> str:
+        return self._filename
+    
+    def get_number_of_frame(self) -> int:
+        return self._num_buffered_frames
