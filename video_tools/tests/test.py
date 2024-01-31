b11f249708f12a74a085a76bf42200d9145fe40e
ElTinmar <martin.privat@hotmail.fr>
2024-01-31 13:50:51 +0100

    boilerplate code for testing

---
+++ video_tools/tests/test.py
@@ -0,0 +1,25 @@
+import unittest
+from video_tools import *
+
+class test_video_reader(unittest.TestCase):
+
+    def test_(self):
+        pass
+
+class test_video_writer(unittest.TestCase):
+
+    def test_(self):
+        pass
+
+class test_background_subtraction(unittest.TestCase):
+    
+    def test_(self):
+        pass   
+
+class test_video_pocessor(unittest.TestCase):
+    
+    def test_(self):
+        pass   
+
+if __name__ == '__main__':
+    unittest.main()
