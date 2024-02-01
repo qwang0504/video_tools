f2cafefb596e96f6e7979d3d08dcb23dbb5cc299
ElTinmar <martin.privat@hotmail.fr>
2024-02-01 13:35:01 +0100

    switch to event based stopping instead of sentinel

--- video_tools/video_display.py
+++ video_tools/video_display.py
@@ -1,16 +1,17 @@
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Event
 import cv2
 import time
 from numpy.typing import NDArray
-
-# TODO test that it works on windows
+from queue import Empty
 
 # TODO I seem to have a problem as soon as a cv2 window 
 # is opened before that, VideoDisplay objects crash
 # (no window display)
 # Workaround for now: create and start the object at
 # the top of the script before anything else
-
+# This seems to be a fork specific issue: looks like
+# forking brings unwanted opencv context that fucks things up
+# I have no issue when forcing to spawn instead
 
 class VideoDisplay(Process):
     def __init__(
@@ -26,6 +27,7 @@ class VideoDisplay(Process):
         self.queue = queue
         self.winname = winname
         self.last_image_time = 0
+        self.stop_event = Event()
 
     def queue_image(self, image: NDArray) -> None:
         '''check fps, discard frames if time not elapsed'''
@@ -33,22 +35,34 @@ class VideoDisplay(Process):
         if ((t - self.last_image_time)*1e-9) >= (1/self.fps):
             self.queue.put(image)
             self.last_image_time = t
+            print('queued')
 
     def exit(self) -> None:
-        self.queue.put(None)
+        self.stop_event.set()
 
     def run(self) -> None:
+
+        # try to reinitialize opencv after fork
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
+
+        print('test0')
         cv2.namedWindow(self.winname)
+        print('test1')
         last_disp_time = time.time_ns()
-        while True:
-            frame = self.queue.get()
-            if frame is None:
-                break
+        while not self.stop_event.is_set():
+            print('test2')
+            try:
+                frame = self.queue.get(timeout=2/self.fps)
+            except Empty:
+                pass
             timestamp = time.time_ns()
             fps_hat = 1/((timestamp - last_disp_time)*1e-9)
             cv2.imshow(self.winname, frame)
-            cv2.displayStatusBar(self.winname,f'Display FPS: {fps_hat:.2f}',1)
+            cv2.displayStatusBar(self.winname, f'Display FPS: {fps_hat:.2f}', 1)
             cv2.waitKey(1)
+            print('disped')
             last_disp_time = time.time_ns()
+
         cv2.destroyWindow(self.winname)
 
