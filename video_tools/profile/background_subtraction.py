dafe3daae4fc34e576bf058471f0c6bfce2f6501
ElTinmar <martin.privat@hotmail.fr>
2024-02-01 18:23:40 +0100

    profile background sub

---
+++ video_tools/profile/background_subtraction.py
@@ -0,0 +1,45 @@
+from video_tools import (
+    InMemory_OpenCV_VideoReader, Polarity,
+    NoBackgroundSub, BackroundImage, InpaintBackground, 
+    StaticBackground, DynamicBackground, DynamicBackgroundMP
+)
+from image_tools import im2single, im2gray
+from tqdm import tqdm
+import cProfile
+import pstats
+
+# video reader
+video_reader = InMemory_OpenCV_VideoReader()
+video_reader.open_file(
+    filename='toy_data/freely_swimming_param.avi',
+    safe=False,
+    memsize_bytes=4e9,
+    single_precision=True,
+    grayscale=True
+)
+num_frames = video_reader.get_number_of_frame()
+
+# background subtraction
+background_sub = BackroundImage(
+    polarity = Polarity.DARK_ON_BRIGHT,
+    image_file_name = 'toy_data/freely_swimming_param.png'
+)
+background_sub.initialize()
+
+with cProfile.Profile() as pr:
+
+    print('Background subtraction ...')
+    for i in tqdm(range(num_frames)):
+        (rval, frame) = video_reader.next_frame()
+        if not rval:
+            raise RuntimeError('VideoReader was unable to read the whole video')
+        
+        # background expects single precision grayscale
+        frame_gray = im2single(im2gray(frame))
+        
+        # background sub
+        background_sub.subtract_background(frame_gray)
+
+video_reader.close()
+ps = pstats.Stats(pr)
+ps.dump_stats('raw_speed.prof')
