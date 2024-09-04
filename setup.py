f96cb5bed393787d61e00369a0d1f74754714f09
ElTinmar <martin.privat@hotmail.fr>
2024-09-04 22:39:58 +0200

    cupy ie cupy-cuda12x

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='video_tools',
     python_requires='>=3.8',
     author='Martin Privat',
-    version='0.3.8',
+    version='0.3.9',
     packages=['video_tools','video_tools.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='simple video reader, writer, and processing functions',
@@ -17,6 +17,6 @@ setup(
         "qt_widgets @ git+https://github.com/ElTinmar/qt_widgets.git@main",
         "image_tools @ git+https://github.com/ElTinmar/image_tools.git@main",
         "tqdm",
-        "cupy-cuda12x"
+        "cupy"
     ]
 )
